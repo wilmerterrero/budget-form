7931bbd79b7ae676912466b4b756e6718129ac0d
chaser2304 <wilmerterrero1922@gmail.com>
2020-06-16 09:46:59 -0400

    editando alert

--- src/components/Swalert.js
+++ src/components/Swalert.js
@@ -4,7 +4,7 @@ import Swal from "sweetalert2";
 const Swalert = ({ cambiarSwal }) => {
   Swal.fire({
     title: "¡Bienvenido!",
-    text: "Con esta Aplicación Web podrás calcular tu Presupuesto.",
+    html: "<h2>Aplicación Web en fase de pruebas, con la que podrás calcular tu Presupuesto Semanal. <b>Si esta aplicación fuera tuya ¿Qué cosas te gustaría incluir?</b></h2>",
     imageUrl:
       "https://www.gestion.org/wp-content/uploads/2014/07/que-es-presupuesto-662x551.jpg",
     imageWidth: 400,
